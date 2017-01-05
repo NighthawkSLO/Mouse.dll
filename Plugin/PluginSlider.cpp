22ff94c4eed90bdff6562a445b095206469d633b
TheAzack9 <TheAzack9@users.noreply.github.com>
2017-01-05 21:18:00 +0100

    Changed handling
    use wndproc instead of threads for better stability

--- Plugin/PluginSlider.cpp
+++ Plugin/PluginSlider.cpp
@@ -6,205 +6,247 @@ To view a copy of this license, visit http://creativecommons.org/licenses/by/4.0
 send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
 */
 
-#include <Windows.h>
-#include <math.h>
-#include <string>
-#include <vector>
-#include <thread>
-#include <cctype>
+// Based on code found here: https://github.com/brianferguson/HotKey.dll/blob/master/PluginHotKey/PluginHotKey.cpp
 
-#include "api\RainmeterAPI.h"
+#include <Windows.h>
+#include <vector>
+#include <algorithm>
+#include "api/RainmeterAPI.h"
+#include <string>
+#include <ctime>
+#include <locale>
+
+struct Measure;
+void RemoveMeasure(Measure* measure);
+
+static std::vector<Measure*> g_MouseMeasures;
+static HINSTANCE g_Instance = nullptr;
+static HHOOK g_Hook = nullptr;
+static bool g_IsHookActive = false;
+LRESULT CALLBACK LLMouseProc(int nCode, WPARAM wParam, LPARAM lParam);
+
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
+{
+	switch (fdwReason)
+	{
+	case DLL_PROCESS_ATTACH:
+		g_Instance = hinstDLL;
+
+		// Disable DLL_THREAD_ATTACH and DLL_THREAD_DETACH notification calls
+		DisableThreadLibraryCalls(hinstDLL);
+		break;
+	}
+
+	return TRUE;
+}
+
+enum MOUSEBUTTON {
+	LEFT,
+	RIGHT,
+	MIDDLE
+};
 
 struct Measure
 {
-    std::wstring MouseButton;
-    std::wstring ClickAction;
-    std::wstring HoldAction;
-    std::wstring DragAction;
-    std::wstring ReleaseAction;
-    std::wstring MoveAction;
+	Measure() : lastMove(clock())
+	{}
+	void* skin;
+	void* rm;
 
-    bool isEnabled;
-    bool isMouseDown;
-    bool isHeld;
-    bool isRelativeToSkin;
+	std::wstring MouseButton;
+	std::wstring ClickAction;
+	std::wstring DragAction;
+	std::wstring ReleaseAction;
+	std::wstring MoveAction;
 
-    int x;
-    int y;
-    int key;
+	bool isEnabled;
+	bool isMouseDown;
+	bool isRelativeToSkin;
+	bool needsFocus;
 
-    clock_t time;
-    double delay;
+	int x;
+	int y;
+	MOUSEBUTTON key;
 
-    void* skin;
-    void* rm;
-
-    Measure() :
-        MouseButton(),
-        ClickAction(), HoldAction(), DragAction(), ReleaseAction(), MoveAction(),
-        isEnabled(), isMouseDown(false), isHeld(false), isRelativeToSkin(),
-        x(), y(), key(),
-        time(), delay(300.0),
-        skin(), rm()
-    { }
+	clock_t lastMove;
+	float moveDelay;
 };
-
-static std::vector<Measure*> g_Measures;
-static bool g_ThreadActive = false;
-void MouseThread();
-void RemoveMeasure(Measure* measure);
 
 PLUGIN_EXPORT void Initialize(void** data, void* rm)
 {
-    Measure* measure = new Measure;
-    *data = measure;
-
-    measure->skin = RmGetSkin(rm);
-    measure->rm = rm;
+	Measure* measure = new Measure;
+	*data = measure;
+	measure->skin = RmGetSkin(rm);
+	measure->rm = rm;
 }
 
 PLUGIN_EXPORT void Reload(void* data, void* rm, double* maxValue)
 {
-    Measure* measure = (Measure*)data;
+	Measure* measure = (Measure*)data;
 
-    measure->MouseButton = RmReadString(rm, L"MouseButton", L"Left");
+	measure->MouseButton = RmReadString(rm, L"MouseButton", L"Left");
 
-    measure->ClickAction = RmReadString(rm, L"ClickAction", L"");
-    measure->HoldAction = RmReadString(rm, L"HoldAction", L"");
-    measure->DragAction = RmReadString(rm, L"DragAction", L"");
-    measure->ReleaseAction = RmReadString(rm, L"ReleaseAction", L"");
-    measure->MoveAction = RmReadString(rm, L"MoveAction", L"");
-    measure->isEnabled = RmReadInt(rm, L"Enabled", 0) == 0 && RmReadInt(rm, L"Paused", 0) == 0;
+	measure->ClickAction = RmReadString(rm, L"ClickAction", L"");
+	measure->DragAction = RmReadString(rm, L"DragAction", L"");
+	measure->ReleaseAction = RmReadString(rm, L"ReleaseAction", L"");
+	measure->MoveAction = RmReadString(rm, L"MoveAction", L"");
+	measure->isEnabled = RmReadInt(rm, L"Enabled", 0) == 0 && RmReadInt(rm, L"Paused", 0) == 0;
 
-    measure->isRelativeToSkin = RmReadInt(rm, L"RelativeToSkin", 1) == 1;
+	measure->isRelativeToSkin = RmReadInt(rm, L"RelativeToSkin", 1) == 1;
+	measure->needsFocus = RmReadInt(rm, L"NeedsFocus", 0) == 1;
+	measure->moveDelay = RmReadInt(rm, L"MoveDelay", 20);
 
-    measure->delay = RmReadDouble(rm, L"HoldDelay", 300.0);
+	if (_wcsnicmp(measure->MouseButton.c_str(), L"left", 4) == 0)
+	{
+		measure->key = LEFT;
+	}
+	else if (_wcsnicmp(measure->MouseButton.c_str(), L"right", 5) == 0)
+	{
+		measure->key = RIGHT;
+	}
+	else if (_wcsnicmp(measure->MouseButton.c_str(), L"middle", 6) == 0)
+	{
+		measure->key = MIDDLE;
+	}
+	else
+	{
+		RmLogF(rm, LOG_ERROR, L"Slider.dll: MouseButton=%s not valid", measure->MouseButton);
+		return;
+	}
 
-    if (_wcsnicmp(measure->MouseButton.c_str(), L"left", 4) == 0)
-    {
-        measure->key = VK_LBUTTON;
-    }
-    else if (_wcsnicmp(measure->MouseButton.c_str(), L"right", 5) == 0)
-    {
-        measure->key = VK_RBUTTON;
-    }
-    else if (_wcsnicmp(measure->MouseButton.c_str(), L"middle", 6) == 0)
-    {
-        measure->key = VK_MBUTTON;
-    }
-    else
-    {
-        RmLogF(rm, LOG_ERROR, L"Slider.dll: MouseButton=%s not valid", measure->MouseButton);
-        return;
-    }
+	if(std::find(g_MouseMeasures.begin(), g_MouseMeasures.end(), measure) == g_MouseMeasures.end())
+	{
+		g_MouseMeasures.push_back(measure);
+	}
 
-    if (std::find(g_Measures.begin(), g_Measures.end(), measure) == g_Measures.end())
-    {
-        g_Measures.push_back(measure);
-    }
-
-    /*
-    This starts the mouse hook
-    */
-    if (g_Measures.size() >= 1 && !g_ThreadActive)
-    {
-        g_ThreadActive = true;
-        std::thread Thread(MouseThread);
-        Thread.detach();
-    }
+	// Start the keyboard hook
+	if (!g_IsHookActive && !g_MouseMeasures.empty())
+	{
+		g_Hook = SetWindowsHookEx(WH_MOUSE_LL, LLMouseProc, g_Instance, NULL);
+		if (g_Hook)
+		{
+			g_IsHookActive = true;
+			RmLogF(rm, LOG_DEBUG, L"MouseHook Started");
+		}
+		else
+		{
+			RmLogF(rm, LOG_ERROR, L"Could not start the mouse hook");
+			RemoveMeasure(measure); 
+		}
+	}
 }
 
 PLUGIN_EXPORT double Update(void* data)
 {
-    Measure* measure = (Measure*)data;
-    return 0.0;
+	Measure* measure = (Measure*)data;
+	return 0.0;
 }
 
 PLUGIN_EXPORT void Finalize(void* data)
 {
-    Measure* measure = (Measure*)data;
-    RemoveMeasure(measure);
-    delete measure;
+	Measure* measure = (Measure*)data;
+	RemoveMeasure(measure);
+	delete measure;
 }
 
 void RemoveMeasure(Measure* measure)
 {
-    std::vector<Measure*>::iterator found = std::find(g_Measures.begin(), g_Measures.end(), measure);
-    if (found != g_Measures.end())
-    {
-        g_Measures.erase(found);
-    }
-    if (g_Measures.empty())
-    {
-        g_ThreadActive = false;
-    }
+	void* rm = measure->rm;
+	std::vector<Measure*>::iterator found = std::find(g_MouseMeasures.begin(), g_MouseMeasures.end(), measure);
+	if (found != g_MouseMeasures.end())
+	{
+		g_MouseMeasures.erase(found);
+	}
+
+	if (g_IsHookActive && g_MouseMeasures.empty())
+	{
+		while (g_Hook && UnhookWindowsHookEx(g_Hook) == FALSE)
+		{
+			RmLogF(rm, LOG_ERROR, L"Could not stop the mouse hook");
+		}
+
+		g_Hook = nullptr;
+		g_IsHookActive = false;
+		RmLogF(rm, LOG_DEBUG, L"MouseHook Stopeed");
+	}
 }
 
 std::wstring wstringReplace(std::wstring wstr, std::wstring oldstr, std::wstring newstr)
 {
-    for (auto& c : wstr) c = std::tolower(c);
-    size_t pos = 0;
-    while ((pos = wstr.find(oldstr, pos)) != std::wstring::npos) {
-        wstr.replace(pos, oldstr.length(), newstr);
-        pos += newstr.length();
-    }
-    return wstr;
+	std::wstring lowerWstr = wstr;
+	std::transform(lowerWstr.begin(), lowerWstr.end(), lowerWstr.begin(), tolower);
+	size_t pos = 0;
+	while ((pos = lowerWstr.find(oldstr, pos)) != std::wstring::npos) {
+		wstr.replace(pos, oldstr.length(), newstr);
+		pos += newstr.length();
+	}
+	return wstr;
 }
 
-void MouseThread()
+LRESULT CALLBACK LLMouseProc(int nCode, WPARAM wParam, LPARAM lParam)
 {
-    while (g_ThreadActive)
-    {
-        POINT p;
-        if (GetCursorPos(&p) && g_Measures.size() >= 1)
-        {
-            clock_t time = clock();
-            for (auto& measure : g_Measures)
-            {
-                if (measure->isEnabled)
-                {
-                    int x = p.x; int y = p.y;
-                    if (measure->isRelativeToSkin)
-                    {
-                        RECT rect;
-                        GetWindowRect(RmGetSkinWindow(measure->rm), &rect);
-                        x -= rect.left; y -= rect.top;
-                    }
-                    std::wstring str_x = std::to_wstring(x); std::wstring str_y = std::to_wstring(y);
-                    if ((GetAsyncKeyState(measure->key) != 0) && !measure->isMouseDown)
-                    {
-                        measure->isMouseDown = true; measure->time = time;
-                        if (!measure->ClickAction.empty()) {
-                            RmExecute(measure->skin, wstringReplace(wstringReplace(measure->ClickAction, L"$mousex$", str_x), L"$mousey$", str_y).c_str());
-                        }
-                    }
-                    if (x != measure->x || y != measure->y)
-                    {
-                        if (!measure->MoveAction.empty()) {
-                            RmExecute(measure->skin, wstringReplace(wstringReplace(measure->MoveAction, L"$mousex$", str_x), L"$mousey$", str_y).c_str());
-                        }
-                        if (measure->isMouseDown && !measure->DragAction.empty()) {
-                            RmExecute(measure->skin, wstringReplace(wstringReplace(measure->DragAction, L"$mousex$", str_x), L"$mousey$", str_y).c_str());
-                        }
-                    }
-                    if ((GetAsyncKeyState(measure->key) == 0) && measure->isMouseDown)
-                    {
-                        measure->isMouseDown = false; measure->isHeld = false;
-                        if (!measure->ReleaseAction.empty()) {
-                            RmExecute(measure->skin, wstringReplace(wstringReplace(measure->ReleaseAction, L"$mousex$", str_x), L"$mousey$", str_y).c_str());
-                        }
-                    }
-                    if (measure->isMouseDown && !measure->isHeld && ((float(time - measure->time) / CLOCKS_PER_SEC) >= (measure->delay / 1000)))
-                    {
-                        measure->isHeld = true;
-                        if (!measure->HoldAction.empty()) {
-                            RmExecute(measure->skin, wstringReplace(wstringReplace(measure->HoldAction, L"$mousex$", str_x), L"$mousey$", str_y).c_str());
-                        }
-                    }
-                    measure->x = x; measure->y = y;
-                }
-            }
-        }
-        Sleep(20);
-    }
+	if(nCode >= 0)
+	{
+
+		MSLLHOOKSTRUCT* msllStruct = reinterpret_cast<MSLLHOOKSTRUCT*>(lParam);
+		POINT mouse = msllStruct->pt; // this fetches the cursor position
+		for (auto& measure : g_MouseMeasures)
+		{
+			if (!measure->isEnabled) continue;
+			if(measure->needsFocus && RmGetSkinWindow(measure->rm) != GetActiveWindow())
+			{
+				continue;
+			}
+			LONG x = mouse.x; LONG y = mouse.y;
+			if (measure->isRelativeToSkin)
+			{
+				RECT rect;
+				GetWindowRect(RmGetSkinWindow(measure->rm), &rect);
+				x -= rect.left; y -= rect.top;
+			}
+			std::wstring str_x = std::to_wstring(x); std::wstring str_y = std::to_wstring(y);
+			if (wParam == WM_LBUTTONDOWN && measure->key == LEFT || wParam == WM_RBUTTONDOWN && measure->key == RIGHT || wParam == WM_MBUTTONDOWN && measure->key == MIDDLE)
+			{
+				if (!measure->isMouseDown)
+				{
+					measure->isMouseDown = true;
+					if (!measure->ClickAction.empty()) 
+					{
+						RmExecute(measure->skin, wstringReplace(wstringReplace(measure->ClickAction, L"$mousex$", str_x), L"$mousey$", str_y).c_str());
+					}
+				}
+			}
+			else if (wParam == WM_LBUTTONUP && measure->key == LEFT || wParam == WM_RBUTTONUP && measure->key == RIGHT || wParam == WM_MBUTTONUP && measure->key == MIDDLE)
+			{
+				if (measure->isMouseDown)
+				{
+					measure->isMouseDown = false;
+					if (!measure->ReleaseAction.empty()) 
+					{
+						RmExecute(measure->skin, wstringReplace(wstringReplace(measure->ReleaseAction, L"$mousex$", str_x), L"$mousey$", str_y).c_str());
+					}
+				}
+			}
+			else if(wParam == WM_MOUSEMOVE)
+			{
+				auto time = clock();
+				float dt = (float)(time - measure->lastMove);
+				if (dt > measure->moveDelay && (x != measure->x || y != measure->y))
+				{
+					if (!measure->MoveAction.empty())
+					{
+						RmExecute(measure->skin, wstringReplace(wstringReplace(measure->MoveAction, L"$mousex$", str_x), L"$mousey$", str_y).c_str());
+					}
+					if (measure->isMouseDown && !measure->DragAction.empty())
+					{
+						RmExecute(measure->skin, wstringReplace(wstringReplace(measure->DragAction, L"$mousex$", str_x), L"$mousey$", str_y).c_str());
+					}
+					measure->lastMove = clock();
+				}
+			}
+			measure->x = x; measure->y = y;
+		}
+	}
+
+	return CallNextHookEx(g_Hook, nCode, wParam, lParam);
 }
